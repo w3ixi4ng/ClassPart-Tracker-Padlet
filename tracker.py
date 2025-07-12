328f8a05c0173b76930e42769eeb2e92ef4bf0a3
weixiang <weixiang0124@gmail.com>
2025-07-12 21:07:11 +0800

    Remove the repetitive calling of cs.csv

--- tracker.py
+++ tracker.py
@@ -3,7 +3,7 @@ import os
 import zipfile
 
 
-def process_csv(results_file, filepath):
+def process_csv(cp_dict, filepath):
     # read post csv file, getting only students' name
     try: 
         df = pd.read_csv(filepath)
@@ -12,45 +12,20 @@ def process_csv(results_file, filepath):
         print(f"Skipping file: {filepath} due to {error}")
         return
 
-    # second post.csv load onwards
-    if os.path.exists(results_file):
-        cp_dict = {}
-
-        # get all students' name and cp in a dict
-        with open(results_file, 'r') as records:
-            for record in records:
-                record = record.rstrip()
-                name, cp = record.split(',')
-                cp_dict[name] = cp
-
-        # ensure all current and new students participating are recorded and only recorded once
-        recorded = set()
-        for student in students:
-            if student not in recorded:
-                if student in cp_dict:
-                    # participated students
-                    cp_dict[student] = int(cp_dict[student]) + 1
-                else:
-                    # new participation
-                    cp_dict[student] = 1
-            recorded.add(student)
-        
-        # write back into csv file
-        with open(results_file, 'w') as records:
-            for name, cp in cp_dict.items():
-                records.write(f"{name},{cp}\n")
-
-    # first Posts .csv load - students only gain 1 CP and only recorded once
-    else:
-        recorded = set()
-        with open(results_file, 'w') as records:
-            for student in students:
-                if student not in recorded:
-                    records.write(student  + ",1" + "\n")
-                recorded.add(student)
+    # ensure all current and new students participating are recorded and only recorded once
+    recorded = set()
+    for student in students:
+        if student not in recorded:
+            if student in cp_dict:
+                # participated before students
+                cp_dict[student] = int(cp_dict[student]) + 1
+            else:
+                # new participation
+                cp_dict[student] = 1
+        recorded.add(student)
 
 
-def process_files(zip_files_folder, results_file):
+def process_files(zip_files_folder, cp_dict):
     # dynamically unzip the zipfiles and process only the Post csv
     for zip_filename in os.listdir(zip_files_folder):
         if zip_filename.endswith('.zip'):
@@ -65,7 +40,7 @@ def process_files(zip_files_folder, results_file):
                 if filename.startswith("Posts") and filename.endswith(".csv"):
                     # call process_csv to process every Posts .csv file
                     filepath = os.path.join(extract_path, filename)
-                    process_csv(results_file, filepath)
+                    process_csv(cp_dict, filepath)
 
 
 def table(results_file):
@@ -77,9 +52,15 @@ def table(results_file):
 def main():
     results_file = 'cp.csv'
     zip_files_folder = 'add_zipfiles_here'
+    cp_dict = {}
 
     # process zipfiles and its corresponding Posts .csv file
-    process_files(zip_files_folder, results_file)
+    process_files(zip_files_folder, cp_dict)
+
+    with open(results_file, 'w') as f:
+        for name, cp in cp_dict.items():
+            f.write(f"{name},{cp}\n")
+
 
     # final call to show results
     if os.path.exists(results_file):
